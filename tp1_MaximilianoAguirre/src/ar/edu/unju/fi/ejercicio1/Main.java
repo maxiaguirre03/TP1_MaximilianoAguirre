cc149f3d7f8d2645544b55b8d42d10dd01a33eb1
maxag <maxag@LAPTOP-QT03V4M7>
2024-04-04 18:04:39 -0300

    ejercicio 1 completo

--- tp1_MaximilianoAguirre/src/ar/edu/unju/fi/ejercicio1/Main.java
+++ tp1_MaximilianoAguirre/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -3,7 +3,15 @@ package ar.edu.unju.fi.ejercicio1;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int n1 = 8;
+		int n2 = 5;
+		int n3 = 1;
+		int n4 = 20;
+		int n5 = 9;
+		double promedio = (double)(n1 + n2 + n3 + n4 + n5)/5;
+		System.out.println(n1+", "+n2+", "+ n3+", "+n4+", "+n5);
+		System.out.println("el promedio es: "+promedio );
+		
 		
 
 	}

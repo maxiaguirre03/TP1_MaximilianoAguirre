7cdb0d126576e803433231ae7a2f56efea7d23ce
maxag <maxag@LAPTOP-QT03V4M7>
2024-04-09 12:41:53 -0300

    segundo punto tp

--- tp1_MaximilianoAguirre/src/ar/edu/unju/fi/ejercicio2/Main.java
+++ tp1_MaximilianoAguirre/src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -3,8 +3,19 @@ package ar.edu.unju.fi.ejercicio2;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String nombreP = "Argentina";
+		int Edad = 24;
+		float altura = 257;
+		double precio = 18899;
+		String numeroCel = "3883101555";
+		double calcCos = Math.cos(0.5);
 		
+		System.out.println("el nombre del pais es: "+nombreP);
+		System.out.println("la edad es: "+Edad);
+		System.out.println("altura es: "+altura);
+		System.out.println("el precio es: "+precio);
+		System.out.println("su numero es: "+numeroCel);
+		System.out.println("el calculo del coseno es: "+calcCos);
 
 	}
 
